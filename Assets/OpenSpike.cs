b24ca301530f0442899fc697cafa4d468bd588ef
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2018-11-30 12:08:53 +0100

    hej

---
+++ Assets/OpenSpike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSpike : MonoBehaviour
+{
+    public Animator anim;
+    private void Start()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            anim.GetBool("Hit");
+        }
+    }
+}
